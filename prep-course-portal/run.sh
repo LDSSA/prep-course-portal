93e1bbeb4344890cad52b5def003542b289cee2c
buedaswag <miguel.dias.1212@gmail.com>
2022-03-26 21:37:20 +0000

    adding default admin user

--- prep-course-portal/run.sh
+++ prep-course-portal/run.sh
@@ -4,7 +4,8 @@
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-# python manage.py runserver 0.0.0.0:8000
+# create admin user if not exists
+python manage.py initadmin
 
 # run the project
 gunicorn prep-course-portal.wsgi:application -b 0.0.0.0:8000
