21b46c99252d605fb808c5131a8734729c462c37
Gustavo-SF <gustavo.fonseca@outlook.pt>
2024-05-10 18:23:09 +0100

    improve admin & serializer

--- prep-course-portal/submissions/admin.py
+++ prep-course-portal/submissions/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from submissions import models
 
-admin.site.register(models.Submission)
+# Submission admin interface
+class SubmissionAdmin(admin.ModelAdmin):
+    list_display = ('created', 'slackid', 'learning_unit', 'exercise_notebook', 'score')
+    list_filter = ('created', 'learning_unit', 'exercise_notebook')
+    search_fields = ('slackid', 'learning_unit', 'exercise_notebook')
+
+admin.site.register(models.Submission, SubmissionAdmin)
--- prep-course-portal/submissions/serializers.py
+++ prep-course-portal/submissions/serializers.py
@@ -8,3 +8,14 @@ class SubmissionSerializer(serializers.ModelSerializer):
         model = Submission
         fields = ('id', 'created', 'slackid',
                   'learning_unit', 'exercise_notebook', 'score')
+        
+    # Make a limit to the learning_unit and score allowed
+    def validate_learning_unit(self, value):
+        if value < 1 or value > 10:
+            raise serializers.ValidationError("Learning unit must be between 1 and 10")
+        return value
+    
+    def validate_score(self, value):
+        if value < 0 or value > 20:
+            raise serializers.ValidationError("Score must be between 0 and 100")
+        return value
