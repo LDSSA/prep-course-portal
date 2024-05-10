082bb1dfe9e3a1cea158a3f150797ec25a6d564c
Gustavo-SF <gustavo.fonseca@outlook.pt>
2024-05-10 21:09:27 +0100

    update validation values

--- prep-course-portal/submissions/serializers.py
+++ prep-course-portal/submissions/serializers.py
@@ -11,11 +11,11 @@ class SubmissionSerializer(serializers.ModelSerializer):
         
     # Make a limit to the learning_unit and score allowed
     def validate_learning_unit(self, value):
-        if value < 1 or value > 10:
-            raise serializers.ValidationError("Learning unit must be between 1 and 10")
+        if value < 0 or value > 18:
+            raise serializers.ValidationError("Learning unit must be between 1 and 18")
         return value
     
     def validate_score(self, value):
         if value < 0 or value > 20:
-            raise serializers.ValidationError("Score must be between 0 and 100")
+            raise serializers.ValidationError("Score must be between 0 and 20")
         return value
